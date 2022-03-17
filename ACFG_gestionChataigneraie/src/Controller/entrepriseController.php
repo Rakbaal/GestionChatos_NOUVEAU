3e772596285f4caed35b7cc770e7c2ecfed692df
victor-fer <victor.fer@campus-la-chataigneraie.org>
2022-03-17 08:19:18 +0100

    Premier commit

---
+++ ACFG_gestionChataigneraie/src/Controller/entrepriseController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Entreprise;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
+
+class entrepriseController extends AbstractController {
+
+    /**
+     * @Route("listeEntreprises", name="listeEntreprises")
+     */
+    function listeEntreprises(ManagerRegistry $doctrine)
+    {
+        $entityManager = $doctrine->getManager();
+        //$listeEntreprises = $entityManager->getRepository(Entreprise::class)->listeAllEntreprises();
+        
+        //return new Response('Bonjour');
+
+        return $this->render('listeEntreprises.html.twig'/*, ['listeEntreprises' => $listeEntreprises]*/);
+    }
+}
+?>
--- ACFG_gestionChataigneraie/src/Controller/loginController.php
+++ ACFG_gestionChataigneraie/src/Controller/loginController.php
@@ -7,31 +7,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Doctrine\Persistence\ManagerRegistry;
 
 class loginController extends AbstractController {
     /**
-     * @Route("/login", name="login")
+     * @Route("login", name="login")
      */
-    public function Login(Request $request, ManagerRegistry $doctrine) : Response {
+    function test(Request $request, ManagerRegistry $doctrine) {
         $utilisateur = new Utilisateur();
         $form = $this->createForm(utilisateurType :: class, $utilisateur);
+        $entityManager = $doctrine->getManager();
+        $login = $utilisateur->getUTILOGIN();
+        $mdp = $utilisateur->getUTIMDP();
 
         $form->handleRequest($request);
         
         if($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-
-            // $entityManager = $doctrine->getManager();
-            // $exist = $entityManager->getRepository(Utilisateur :: class)->findOneBy(['uti_login' => $data->getUTILOGIN()]) != null;
             
-            if (true) {
-                $request->getSession()->set('login', $data->getUTILOGIN());
-                return $this->redirect("accueil");
+            $test = 'Utilisateur inexistant';
+            
+            if($entityManager->getRepository(Utilisateur :: class)->findOneBy(['uti_login' => $login])) {
+                $session = new Session();
+                $session->start();
+                $session->set('login', $login);
+                $test = 'Bonjour '.$login;
             }
-            else {
-                return new Response("L'utilisateur n'existe pas");
-            }
+
+            return new Response($test);
         }
         
         return $this->render('login.html.twig', ['form' => $form->createView()]);
