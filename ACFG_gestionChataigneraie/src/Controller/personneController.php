ad209fe5624d7f380e3428642347e887fe9682a2
theoGarin <theo.garin@campus-la-chataigneraie.org>
2022-03-28 12:01:10 +0200

    Lien entre la page listePersonne et infoentreprise

--- ACFG_gestionChataigneraie/src/Controller/personneController.php
+++ ACFG_gestionChataigneraie/src/Controller/personneController.php
@@ -5,13 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Personne;
+use App\Entity\Entreprise;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 
 class personneController extends AbstractController {
 
     /**
-     * @Route("listePersonnes", name="listeListePersonnes")
+     * @Route("listePersonnes", name="listePersonnes")
      */
     function listePersonnes(ManagerRegistry $doctrine)
     {
