425f1f7f18dc6b0698fe7bbea7e4bd0470812fe7
Rakbaal <leocarp76@outlook.fr>
2022-04-14 15:41:22 +0200

    Harmonisation du code:
    - Ajout du twig pour les updates
    - Rassemblement des controllers

--- ACFG_gestionChataigneraie/src/Controller/entrepriseController.php
+++ ACFG_gestionChataigneraie/src/Controller/entrepriseController.php
@@ -67,5 +67,26 @@ class entrepriseController extends AbstractController {
         
         return $this->redirect($this->generateUrl('listeEntreprises'));
     }
+
+    /**
+     * @Route("/infoEntreprise/{id}", name="infoEntreprise")
+     */
+    public function infoEntreprise($id, ManagerRegistry $doctrine, Request $request){
+        $session = $request->getSession();
+        $entityManager = $doctrine->getManager();
+        $Entreprise = $entityManager->getRepository(Entreprise::class)->find($id);
+        $listePersonne = $Entreprise->getPersonnes();
+
+        if ($session->get('login')) {
+            return $this->render("infoEntreprise.html.twig ", [
+                "Entreprises" => $Entreprise,
+                "listePersonne" => $listePersonne,
+                "admin" => $session->get('admin')
+            ]);
+        } else {
+            return $this->render("erreurAcces.html.twig");
+        }
+        
+    }
 }
 ?>
