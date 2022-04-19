cf39127846ca0e134ce7435da3341b88a3f53035
victor-fer <victor.fer@campus-la-chataigneraie.org>
2022-04-19 15:01:46 +0200

    Filtre quasiment finis

--- ACFG_gestionChataigneraie/src/Controller/entrepriseController.php
+++ ACFG_gestionChataigneraie/src/Controller/entrepriseController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Entreprise;
-use App\Form\entrepriseCompletType;
+use App\Form\entrepriseType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,45 +15,43 @@ class entrepriseController extends AbstractController {
     /**
      * @Route("/listeEntreprises", name="listeEntreprises")
      */
-    function listeEntreprises(ManagerRegistry $doctrine, Request $request){
+    function listeEntreprises(ManagerRegistry $doctrine, Request $request)
+    {
+        // Execute une requête pour afficher toutes les entreprises dans le tableau
         $session = $request->getSession();
         $entityManager = $doctrine->getManager();
         $listeEntreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+        
 
+        // Création du formulaire pour le filtrage
         $entreprise = new Entreprise();
-        $form = $this->createForm(entrepriseCompletType :: class, $entreprise);
-        
+        $form = $this->createForm(entrepriseType :: class, $entreprise);
         $form->handleRequest($request);
-        
+
+        // Si l'utilisateur clique sur 'Rechercher'
         if($form->isSubmitted() && $form->isValid()) {
+            // Récupération des valeurs des textbox saisies
             $data = $form->getData();
-            $entityManager->persist($data);
-            $entityManager->flush();
 
-            return $this->redirect($this->generateUrl("listeEntreprises"));
-        }
-
-        if ($session->get('login')) {
+            // Execute une requête pour afficher les entreprises respectant les critères voulus
+            $entityManager = $doctrine->getManager();
+            $listeEntreprisesFiltre = $entityManager->getRepository(Entreprise::class)->findByEntrepriseFiltre($data/*$data->getENTRS(),  $data->getENTVILLE(), $data->getENTPAYS() , $data->getSpecialites()*/);
+        
+            // Retourne la liste des entreprises concernées par les critères
             return $this->render('listeEntreprises.html.twig', [
                 'form' => $form->createView(),
-                'listeEntreprises' => $listeEntreprises,
+                'listeEntreprises' => $listeEntreprisesFiltre,
                 'admin' => $session->get('admin')
             ]);
-        } else {
-            return $this->render("erreurAcces.html.twig");
         }
-        
-    }
 
-    /**
-     * @Route("listeEntreprisesFiltre/{rs}{ville}{pays}{specialite}", name="listeEntreprisesFiltre")
-     */
-    function listeEntreprisesFiltre(ManagerRegistry $doctrine, Request $request)
-    {
-        $session = $request->getSession();
-        $entityManager = $doctrine->getManager();
-        $listeEntreprises = $entityManager->getRepository(Entreprise::class)->findAll();
-        return $this->render('listeEntreprises.html.twig', ['listeEntreprises' => $listeEntreprises, 'admin' => $session->get('admin')]);
+        // Retourne la liste entière des entreprises
+        return $this->render('listeEntreprises.html.twig', [
+            'form' => $form->createView(),
+            'listeEntreprises' => $listeEntreprises,
+            'admin' => $session->get('admin')
+        ]);
+
     }
 
     /**
