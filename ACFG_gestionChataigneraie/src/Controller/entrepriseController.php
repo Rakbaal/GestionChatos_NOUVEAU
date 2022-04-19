e78b8e93d851b7f2f12f55a409d92ab0a8e4e80d
victor-fer <victor.fer@campus-la-chataigneraie.org>
2022-04-19 15:12:38 +0200

    Modification de EntrepriseController avec les deux formulaires

--- ACFG_gestionChataigneraie/src/Controller/entrepriseController.php
+++ ACFG_gestionChataigneraie/src/Controller/entrepriseController.php
@@ -17,40 +17,69 @@ class entrepriseController extends AbstractController {
      */
     function listeEntreprises(ManagerRegistry $doctrine, Request $request)
     {
-        // Execute une requête pour afficher toutes les entreprises dans le tableau
         $session = $request->getSession();
+
+        // Execute une requête pour afficher toutes les entreprises dans le tableau
         $entityManager = $doctrine->getManager();
         $listeEntreprises = $entityManager->getRepository(Entreprise::class)->findAll();
         
 
         // Création du formulaire pour le filtrage
         $entreprise = new Entreprise();
-        $form = $this->createForm(entrepriseType :: class, $entreprise);
-        $form->handleRequest($request);
+        $formFiltre = $this->createForm(entrepriseType :: class, $entreprise);
+        $formFiltre->handleRequest($request);
+
+        // Création du formulaire pour l'ajout d'une entreprise
+        $entreprise2 = new Entreprise();
+        $formNouveau = $this->createForm(entrepriseCompletType :: class, $entreprise2);
+        $formNouveau->handleRequest($request);
 
         // Si l'utilisateur clique sur 'Rechercher'
-        if($form->isSubmitted() && $form->isValid()) {
+        if($formFiltre->isSubmitted() && $formFiltre->isValid()) {
             // Récupération des valeurs des textbox saisies
-            $data = $form->getData();
+            $data = $formFiltre->getData();
 
             // Execute une requête pour afficher les entreprises respectant les critères voulus
             $entityManager = $doctrine->getManager();
-            $listeEntreprisesFiltre = $entityManager->getRepository(Entreprise::class)->findByEntrepriseFiltre($data/*$data->getENTRS(),  $data->getENTVILLE(), $data->getENTPAYS() , $data->getSpecialites()*/);
+            $listeEntreprisesFiltre = $entityManager->getRepository(Entreprise::class)->findByEntrepriseFiltre($data);
         
             // Retourne la liste des entreprises concernées par les critères
             return $this->render('listeEntreprises.html.twig', [
-                'form' => $form->createView(),
+                'formFiltre' => $formFiltre->createView(),
+                'formNouveau' => $formNouveau->createView(),
                 'listeEntreprises' => $listeEntreprisesFiltre,
                 'admin' => $session->get('admin')
             ]);
         }
+        
+        if($formNouveau->isSubmitted() && $formNouveau->isValid()) {
+            $data = $formNouveau->getData();
+            $entityManager->persist($data);
+            $entityManager->flush();
 
+            return $this->redirect($this->generateUrl("listeEntreprises"));
+        }
+
+        if ($session->get('login')) {
+            return $this->render('listeEntreprises.html.twig', [
+                'formFiltre' => $formFiltre->createView(),
+                'formNouveau' => $formNouveau->createView(),
+                'listeEntreprises' => $listeEntreprises,
+                'admin' => $session->get('admin')
+            ]);
+        } else {
+            return $this->render("erreurAcces.html.twig");
+        }
+
+
+        /*
         // Retourne la liste entière des entreprises
         return $this->render('listeEntreprises.html.twig', [
-            'form' => $form->createView(),
+            'form' => $formFiltre->createView(),
             'listeEntreprises' => $listeEntreprises,
             'admin' => $session->get('admin')
         ]);
+        */
 
     }
 
